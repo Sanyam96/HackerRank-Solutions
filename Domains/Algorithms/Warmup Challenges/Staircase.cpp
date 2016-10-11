f0ec90602184151006d4b0e1e6e0253e7a008f12
sanyam96 <you@example.com>
2016-10-12 00:54:27 +0530

    Algorithms - Warmup Challenges - Staircase

---
+++ Domains/Algorithms/Warmup Challenges/Staircase.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    for( int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            cout << " ";
+        }
+        for(int j = 0; j <= i; j++){
+            cout << "#";
+        }
+        cout << endl;
+    }
+    return 0;
+}
