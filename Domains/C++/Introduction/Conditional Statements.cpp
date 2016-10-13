4d909162e4762da4f5d9c4aa2b4d43f5d69920b1
sanyam96 <you@example.com>
2016-10-13 15:32:38 +0530

    C++ - Introduction - Conditional Statements

---
+++ Domains/C++/Introduction/Conditional Statements.cpp
@@ -0,0 +1,58 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int no;
+    cin >> no;
+
+    if( no == 1)
+    {
+        cout << "one" << endl;
+    }
+    else if( no == 2 )
+    {
+        cout << "two" << endl;
+    }
+    else if( no == 3 )
+    {
+        cout << "three" << endl;
+    }
+    else if( no == 4 )
+    {
+        cout << "four" << endl;
+    }
+    else if( no == 5 )
+    {
+        cout << "five" << endl;
+    }
+    else if( no == 6 )
+    {
+        cout << "six" << endl;
+    }
+    else if( no == 7 )
+    {
+        cout << "seven" << endl;
+    }
+    else if( no == 8 )
+    {
+        cout << "eight" << endl;
+    }
+    else if( no == 9 )
+    {
+        cout << "nine" << endl;
+    }
+    else
+    {
+        cout << "Greater than 9" << endl;
+    }
+
+ 
+   return 0;
+}
