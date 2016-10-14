14e142531ad72f2d38d3bec16bac79a94b966e4b
sanyam96 <you@example.com>
2016-10-14 21:41:06 +0530

    C++ - Introduction - Functions

---
+++ Domains/C++/Introduction/Functions.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int arr[4];
+    arr[0] = a;
+    arr[1] = b;
+    arr[2] = c;
+    arr[3] = d;
+
+    int max = arr[0];
+
+    for( int i = 0; i < 4; i++)
+    {
+        if( arr[i] > max)
+        {
+            max = arr[i];
+        }    
+    }
+    
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
