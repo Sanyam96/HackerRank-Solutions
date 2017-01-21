343c41fed20855de0bdb614f87f379ce90471401
Sanyam Goel <sanyam.bvcoe96@gmail.com>
2017-01-21 16:26:37 +0530

    updated README.md file

---
+++ Domains/C++/Strings/Strings.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   // Complete the program
+    
+    string s1, s2;
+
+    cin >> s1 >> s2;
+
+    cout << s1.size() << " " << s2.size() << endl;
+
+    cout << s1+s2 << endl;
+
+    char a1 = s1[0];
+    char a2 = s2[0];
+
+    s1[0] = a2;
+    s2[0] = a1;
+
+    cout << s1 << " " << s2;  
+    return 0;
+}
