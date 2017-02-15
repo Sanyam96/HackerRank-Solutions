3affe4b3f40fc1ac9caaa5af8ed1ab7e60786edb
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-02-15 20:55:46 +0530

    Mathematics - Fundamentals - Maximum Draws

---
+++ Domains/Mathematics/Fundamentals/Maximum Draws.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+
+    int tc;
+    cin >> tc;
+    
+    while( tc-- )
+    {
+        int n;
+        cin >> n;
+        cout << n+1 << endl;    
+    }
+    return 0;
+}
