ed04b6e5f49d02e7bf281d942d4ef3b0cc9d710b
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-07-01 19:54:24 +0530

    Restaurant/ Fundamentals/ Mathematics

---
+++ Domains/Mathematics/Fundamentals/Restaurant.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int gcd ( int a, int b )
+{
+  int c;
+  while ( a != 0 ) {
+     c = a; a = b%a;  b = c;
+  }
+  return b;
+}
+
+int main(){
+	int testcases;
+	scanf("%d", &testcases);
+	int i;
+	for(i=0; i<testcases;i++){
+		int l;
+		int b;
+		scanf("%d %d", &l, &b);
+		printf("%d\n", (l/gcd(l,b)*b/gcd(l,b)));
+	}
+    return 0;
+}
