f49c560b03a05227b86afff80f47436f4a59c724
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-10-18 22:32:57 +0530

    Beautiful Days at the Movies

---
+++ Domains/Core CS Tracks/Basic Programming/Beautiful Days at the Movies.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int rev(int m){
+    int r=0,rem;
+    while(m != 0)
+    {
+        rem = m%10;
+        r = r*10 + rem;
+        m /= 10;
+    }
+    return r;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int i,j,re,c=0;
+    long int k;
+    cin>>i>>j>>k;
+    for(int s=i; s<=j; s++){
+      double x= (s-rev(s))/double(k);
+        if(floor(x) == x)
+            c++;
+    }
+    cout<<c;
+    return 0;
+}
