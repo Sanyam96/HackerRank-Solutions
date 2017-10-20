553425ad88ffd165f8a8e69501ceadb99f8a314e
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-10-20 20:40:44 +0530

    Vector-Sort Updated

--- Domains/C++/STL/Vector-Sort.cpp
+++ Domains/C++/STL/Vector-Sort.cpp
@@ -1,10 +1,5 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
@@ -12,18 +7,16 @@ int main() {
     vector<int> v;
     int n;
     cin >> n;
+    int x;
     
-    for( int i = 0; i < n; i++)
-    {
-        int x;
+    for( int i = 0; i < n; i++) {
         cin >> x;
         v.push_back(x);
     }
 
     sort(v.begin(),v.end());
 
-    for( int i = 0; i < n; i++)
-    {
+    for( int i = 0; i < n; i++) {
         cout << v[i] << " ";
     }
     cout << endl;
